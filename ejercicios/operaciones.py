2f84c3723f67450da61e7bb08978a9a4fb95df47
solblaze03 <maicolbarragan1993@hotmail.com>
2024-05-08 16:02:08 +0200

    operaciones

--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -3,3 +3,17 @@ def suma(x,y):
 
     # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente
     return x + y
+
+
+def resta(x,y):
+
+    return x - y
+
+def mult(x,y):
+
+    return x * y
+
+def divi(x,y):
+    return x / y
+
+
